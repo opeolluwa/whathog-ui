e4240535ea41d3cc6c868a12931381168c10c1d2
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2023-02-06 17:13:20 +0100

    chore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,27 +1,10 @@
-import { CapacitorConfig } from "@capacitor/cli";
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.whathog.app",
-  appName: "WhatHog",
-  webDir: "./dist",
-  bundledWebRuntime: false,
-  plugins: {
-    /* "SplashScreen": {
-      "launchShowDuration": 3000,
-      "launchAutoHide": true,
-      "backgroundColor": "#000000",
-      "androidSplashResourceName": "splash",
-      "androidScaleType": "CENTER_CROP",
-      "showSpinner": true,
-      "androidSpinnerStyle": "large",
-      "iosSpinnerStyle": "small",
-      "spinnerColor": "#999999",
-      "splashFullScreen": true,
-      "splashImmersive": true,
-      "layoutName": "launch_screen",
-      "useDialog": true
-    } */
-  },
+  appId: 'com.awareness.app',
+  appName: 'awareness',
+  webDir: 'dist',
+  bundledWebRuntime: false
 };
 
 export default config;
